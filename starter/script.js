67fdbbc97cb0cf297e86e63110131c8d7521beec
Kaustubh Rai <98734149+kaustubh0955@users.noreply.github.com>
2023-08-17 23:44:00 +0530

    dom--first

--- starter/script.js
+++ starter/script.js
@@ -29,8 +29,67 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+///////////////////////////////////////////////////////
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    ('height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth)
+  );
+
+  //Scrolling
+  // window.scrollTo(
+  // s1coords.left + window.pageXOffset,
+  // s1coords.top + window.pageYOffset
+  // );
+
+  //window.scrollTo({
+  //left: s1coords.left + window.pageXOffset,
+  //top: s1coords.top + window.pageYOffset,
+  //behavior: 'smooth',
+  //});
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
 
 ///////////////////////////////////////////////////////
+
+//Page Navigation
+
+//document.querySelectorAll('.nav__link').forEach(function (el) {
+//  el.addEventListener('click', function (e) {
+//  e.preventDefault();
+//const id = this.getAttribute('href');
+// console.log(id);
+//document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//});
+//});
+
+//Event Delegation
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  //Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+///////////////////////////////////////////////////////
+/*
 
 console.log(document.documentElement);
 console.log(document.head);
@@ -45,7 +104,6 @@ const allButtons = document.getElementsByTagName('button');
 console.log(allButtons);
 
 console.log(document.getElementsByClassName('btn'));
-
 //Creating and Inserting Elements
 
 //.insertAdjacentHTML
@@ -118,3 +176,45 @@ logo.classList.contains('c');
 
 //Don't use
 logo.className = 'Kaustubh';
+*/
+
+/*
+const h1 = document.querySelector('h1');
+h1.addEventListener('mouseenter', function (e) {
+  alert('addEventListener:Great! You are reading the heading');
+});
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+h1.onmouseenter = function (e) {
+  alert('onmouseenter:Great! You are reading the heading');
+};
+//Bubbling and capturing
+//Event Propagation
+//rgb(255,255,255)
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+
+  //currentTarget is the element in which the even handler is attached
+  //Stop Propagation
+  //e.stopPropagation();
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
+});
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('NAV', e.target, e.currentTarget);
+});
+
+//Event Delegation:Implementing Page Navigation
+
+*/
